a7dee1f1dfd312018886ed7ebdb86cfe1b9b4b8e
EvilMikado <evil.mikado.sultan@gmail.com>
2023-05-18 20:42:17 +0300

    added modal

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const burgerToggle = document.querySelector('#burger__toggle');
+const burgerToggle = document.querySelector('#burger__toggle')
 const burgerLine = document.querySelector('.burger__line')
 const menuItem = document.querySelectorAll('.menu__item')
-const menu = document.querySelector('.menu__list');
+const menu = document.querySelector('.menu__list')
+const modal = document.querySelector('.modal')
+const contactBtn = document.querySelectorAll('.btn')
+const overlay = document.querySelector('.overlay')
+const phoneInput = document.querySelector('.phone-input')
+const modalBtn = document.querySelector('.modal__btn')
+
+phoneInput.value = ''
 
 menuItem.forEach(item=>{
   item.addEventListener('click',()=>{
@@ -15,3 +22,35 @@ burgerToggle.addEventListener('click',()=>{
   menu.classList.toggle('menu_burger_enabled');
 
 })
+
+contactBtn.forEach(item=>{
+  item.addEventListener('click', ()=>{
+    modal.classList.add('modal_visible')
+    overlay.classList.add('overlay_visible')
+  })
+})
+
+overlay.addEventListener('click',()=>{
+  modal.classList.remove('modal_visible')
+  overlay.classList.remove('overlay_visible')
+})
+
+modalBtn.addEventListener('click', ()=>{
+  modal.classList.remove('modal_visible')
+  overlay.classList.remove('overlay_visible')
+})
+
+phoneInput.addEventListener('focusout',()=>{
+  if(phoneInput.value.length < 2)
+    phoneInput.value = ''
+})
+
+phoneInput.addEventListener('focus', _ => {
+  if(!/^\+\d*$/.test(phoneInput.value))
+    phoneInput.value = '+';
+});
+
+phoneInput.addEventListener('keypress', e => {
+  if(!/\d/.test(e.key) || phoneInput.value.length > 11)
+    e.preventDefault();
+});
